19b0efc593e936f530c6675ca73b2b505e8eeb53
Tatyana Vlasova <vlasova.t@divan.ru>
2024-03-17 17:26:09 +0300

    Create the useMatchMadia custom hook

---
+++ src/hooks/useMatchMedia.ts
@@ -0,0 +1,42 @@
+import { useLayoutEffect, useState } from "react";
+
+type Device = "isDesktop" | "isTablet" | "isMobile";
+
+type MediaQueries = { [key in Device]: string };
+type MatchedMedia = { [key in Device]: boolean };
+
+const mediaQueries: MediaQueries = {
+  isDesktop: "(min-width: 1281px)",
+  isTablet: "(min-width: 768px) and (max-width: 1280px)",
+  isMobile: "(max-width: 767px)",
+};
+
+export function useMatchMedia(): MatchedMedia {
+  const [matchedMedia, setMatchedMedia] = useState<MatchedMedia>({
+    isDesktop: false,
+    isTablet: false,
+    isMobile: false,
+  });
+
+  useLayoutEffect(() => {
+    function handleResize() {
+      const entries = Object.entries(mediaQueries).map(([device, query]) => {
+        const match = window.matchMedia(query).matches;
+
+        return [device, match];
+      });
+
+      const matchedMedia: MatchedMedia = Object.fromEntries(entries);
+
+      setMatchedMedia(matchedMedia);
+    }
+
+    setTimeout(handleResize);
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return matchedMedia;
+}
