3eca0f9202faf158796c3906e5f811ad4e5e5f84
Tatyana Vlasova <vlasova.t@divan.ru>
2024-06-11 23:00:47 +0300

    Fix layout error and change cube size

--- src/pages/ContactsPage/ContactsPage.tsx
+++ src/pages/ContactsPage/ContactsPage.tsx
@@ -1,15 +1,21 @@
 import VariableTitle from 'components/VariableTitle';
 import Cube from 'components/Cube';
 
+import { useMatchMedia } from 'hooks';
+
 import styles from './ContactsPage.module.css';
 
 import type { FC } from 'react';
 
 const ContactsPage: FC = () => {
+  const { isMobile, isTablet } = useMatchMedia();
+
   return (
     <div className={styles.page}>
       <VariableTitle text="Contacts" fontSizeInVW={20} />
-      <Cube className={styles.cube} size={300} />
+
+      {/* TODO: переделать логику указания размера - неудобно */}
+      <Cube className={styles.cube} size={isTablet ? 200 : isMobile ? 120 : 300} />
     </div>
   );
 };
